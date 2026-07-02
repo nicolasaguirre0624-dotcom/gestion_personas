ce24ed08db7a461bf6b3d67b2ffbe66797d98a82
Erick nicolas <nicolasaguirre0624@gmail.com>
2026-07-02 17:33:43 -0500

    metodo constructor

--- public/index.php
+++ public/index.php
@@ -1,20 +1,15 @@
 <?php
 require_once "../clases/persona.php";
+require_once "../clases/estudiante.php";
+require_once "../clases/producto.php";
 
 $persona1 = new Persona("erick", "gonzalez", 20, "nicolasguirre0624@gmail.com");
 $persona2 = new Persona("nicolas", "aguirre", 20, "nicolasaguirre@gmail.com");
-$estudiante1 = new estudiante("juan", "perez", 20, "juanperez@gmail.com");
+$estudiante1 = new Estudiante("juan", "perez", 20, "juanperez@gmail.com");
+$carro1 = new carro("Toyota", "Corolla", 2020, "Rojo");
 
 echo $persona1->obtenerDatos();
 echo $persona2->obtenerDatos();
-echo $estudiante1->obtenerDatos();
-
-require_once "../clases/producto.php";
-
-$carro1 = new carro("Toyota", "Corolla", 2020, "Rojo");
-
 echo $carro1->obtenerDatos();
-
-require_once "../clases/estudiante.php";
-$estudiante1 = new estudiante("Juan", "Pérez", 20, "juanperez@gmail.com");
 echo $estudiante1->obtenerDatos();
+
